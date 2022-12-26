080fc2a6b6b3f53387a5bdd3884b70359c774838
Yoo <santiagofrieza@gmail.com>
2022-12-26 14:45:38 -0500

    Data settings Refactor

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,50 @@
 import { App, top, mid } from "./App.js";
 
-window.addEventListener('load', async ()=>{
-  
-  App.getCoordinates()
+window.addEventListener("load", async () => {
+  App.getCoordinates();
 
-  const plusIconSrc = "../css/icons/plus.png"
-  const menuIconSrc = "../css/icons/menu.png"
-  const statusIconSrc = "../css/icons/status.png"
+  const plusIconSrc = "../css/icons/plus.png";
+  const menuIconSrc = "../css/icons/menu.png";
+  const statusIconSrc = "../css/icons/status.png";
 
-  top.plusIcon.src = plusIconSrc
-  top.menuIcon.src = menuIconSrc
+  top.plusIcon.src = plusIconSrc;
+  top.menuIcon.src = menuIconSrc;
 
   // mid.statusIcon
 
-  setTimeout(() => {
-    //Make use of App.LocalResponse here
-    console.log(App.LocalResponse)
-    top.City.textContent = App.LocalResponse.name
+  setWeather(App);
+});
 
-    mid.Status.textContent = App.LocalResponse.weather[0].main
-    mid.Temperature.textContent = Math.round(App.LocalResponse.main.temp - 273.15) + "°"
-    mid.MaxTemp.textContent =  Math.round(App.LocalResponse.main.temp_max - 273.15) + "°C"
-    mid.MinTemp.textContent =  Math.round(App.LocalResponse.main.temp_min - 273.15) + "°C"
-  }, 4000)
+function setWeather(response) {
+  
+  setTimeout(() => {
+    //Make use of App.cityResponse here
+    top.City.textContent = response.cityResponse.name;
+
+    mid.Status.textContent = response.cityResponse.weather[0].main;
+    mid.Temperature.textContent =
+      Math.round(response.cityResponse.main.temp - 273.15) + "°";
+    mid.MaxTemp.textContent =
+      Math.round(response.cityResponse.main.temp_max - 273.15) + "°C";
+    mid.MinTemp.textContent =
+      Math.round(response.cityResponse.main.temp_min - 273.15) + "°C";
+  }, 2000);
+}
+
+const showInputButton = top.City;
+const inputText = document.getElementById("city-input");
+
+showInputButton.addEventListener("click", () => {
+  if (inputText.style.display === "none") {
+    inputText.style.display = "block";
+
+    inputText.addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        App.getWeather(inputText.value);
+        setWeather(App);
+        inputText.style.display = "none";
+        inputText.value = ""
+      }
+    });
+  }
 });
