f491881de1adabbaa624e47cafe0fca80e98ad60
Yoo <santiagofrieza@gmail.com>
2022-12-26 10:31:04 -0500

    Mid of the UI

---
+++ JS/UI/Mid.js
@@ -0,0 +1,17 @@
+const Mid = ()=> {
+
+    const Status = document.getElementById("status");
+    const Temperature = document.getElementById("temperature");
+    const MinTemp = document.getElementById("min-temp");
+    const MaxTemp = document.getElementById("max-temp");
+
+    return{
+        Status,
+        Temperature,
+        MinTemp,
+        MaxTemp
+    }
+
+}
+
+export default Mid;
