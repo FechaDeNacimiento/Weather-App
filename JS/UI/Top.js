f28e91c253207eb4778fcc943f819747aa13db82
Yoo <santiagofrieza@gmail.com>
2022-12-26 10:30:45 -0500

    Top of the UI

---
+++ JS/UI/Top.js
@@ -0,0 +1,17 @@
+const Top = () => {
+
+  const Header = document.getElementById("Top");
+  const Mid = document.getElementById("Mid");
+  const Bottom = document.getElementById("Bottom");
+
+  const City = document.getElementById("city");
+
+  return {
+    Header,
+    Mid,
+    Bottom,
+    City
+  };
+};
+
+export default Top;
