5c25fb4fb0005930446f44ab3d4541c20a3fb18c
Yoo <santiagofrieza@gmail.com>
2022-12-27 14:55:13 -0500

    Support for icons added (Refactor necessary)

--- JS/main.js
+++ JS/main.js
@@ -20,6 +20,12 @@ function setWeather(response) {
     //Make use of App.cityResponse here
     top.City.textContent = response.cityResponse.name;
 
+    const icon = response.cityResponse.weather[0].icon
+    const img = document.getElementById("icon")
+
+    img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+
     mid.Status.textContent = response.cityResponse.weather[0].main;
     mid.Temperature.textContent =
       Math.round(response.cityResponse.main.temp - 273.15) + "°";
@@ -27,5 +33,13 @@ function setWeather(response) {
       Math.round(response.cityResponse.main.temp_max - 273.15) + "°C";
     mid.MinTemp.textContent =
       Math.round(response.cityResponse.main.temp_min - 273.15) + "°C";
+
+      console.log(response.cityResponse);
+
   }, 2000);
 }
+
+App.getWeather("london")
+setTimeout(()=>{
+  console.log(App)
+}, 2000)
